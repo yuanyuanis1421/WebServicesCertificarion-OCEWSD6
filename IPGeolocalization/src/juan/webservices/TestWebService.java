ced416cb59f48a5ae251504bcff6a2960bb1ea08
Juan Ibañez Blanco <yuanyuanis@yahoo.es>
2016-11-05 18:32:04 +0100

    Añadidos comentarios

--- IPGeolocalization/src/juan/webservices/TestWebService.java
+++ IPGeolocalization/src/juan/webservices/TestWebService.java
@@ -5,9 +5,15 @@ import net.webservicex.GlobalWeatherSoap;
 
 public class TestWebService {
 
+	/**
+	 * Este método prueba un Servicio Web Service
+	 * @param args
+	 */
 	public static void main(String... args) {
 		GlobalWeather globalWeather = new GlobalWeather();
 		GlobalWeatherSoap globalWeatherSoap = globalWeather.getGlobalWeatherSoap();
 		System.out.println(globalWeatherSoap.getCitiesByCountry("spain"));
+		
+		
 	}
 }
